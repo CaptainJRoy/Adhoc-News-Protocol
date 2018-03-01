131910366eeccc83dd2dec39d71020adc9fcf213
João Miguel <jrsmiguel@outlook.pt>
2018-03-01 16:13:54 +0000

    Ipv6 probe

--- adhoc_app.py
+++ adhoc_app.py
@@ -17,27 +17,17 @@ import time
 import struct
 import socket
 import sys
-
-def main():
-    group = MYGROUP_6
-
-    if "-s" in sys.argv[1:]:
-        sender(group)
-    else:
-        receiver(group)
+import thread
 
 
-def sender(group):
-    addrinfo = socket.getaddrinfo(group, None)[0]
-
+def sender_udp():
+    global MYGROUP_6
+    addrinfo = socket.getaddrinfo(MYGROUP_6, None)[0]
     s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
     # Set Time-to-live (optional)
     ttl_bin = struct.pack('@i', MYTTL)
-    if addrinfo[0] == socket.AF_INET: # IPv4
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-    else:
-        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+    s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
 
     while True:
         data = repr(time.time())
@@ -45,11 +35,25 @@ def sender(group):
         time.sleep(1)
 
 
-def receiver(group):
-    # Look up multicast group address in name server and find out IP version
-    addrinfo = socket.getaddrinfo(group, None)[0]
 
-    # Create a socket
+def sender_tcp():
+    global MYGROUP_6
+
+    #STREAM - TCP
+    s = socket.socket(addrinfo[0], socket.SOCK_STREAM)
+    ttl_bin = struct.pack('@i', MYTTL)
+    #verificar IPv6 target
+    s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+    while True:
+        data = repr(time.time())
+        s.sendto(data + '\0', (addrinfo[4][0], MYPORT))
+        time.sleep(1)
+
+
+
+def receiver():
+    global MYGROUP_6
+    addrinfo = socket.getaddrinfo(MYGROUP_6, None)[0]
     s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
     # Allow multiple copies of this program on one machine
@@ -60,20 +64,18 @@ def receiver(group):
     s.bind(('', MYPORT))
 
     group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
-    # Join group
-    if addrinfo[0] == socket.AF_INET: # IPv4
-        mreq = group_bin + struct.pack('=I', socket.INADDR_ANY)
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    else:
-        mreq = group_bin + struct.pack('@I', 0)
-        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+    # Join MYGROUP_6
+
+    mreq = group_bin + struct.pack('@I', 0)
+    s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
     # Loop, printing any data we receive
     while True:
         data, sender = s.recvfrom(1500)
         while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
-        print (str(sender) + '  ' + repr(data))
+        print ('Received: ' + str(sender) + '  ' + repr(data))
 
 
 if __name__ == '__main__':
-    main()
+    t1 = thread.start_new_thread(sender_udp, ())
+    receiver()
